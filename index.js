c21ece582535492e1455d48f33b30e8e3b2d0a83
Diego Mendes <mendesdiego823@gmail.com>
2021-05-01 11:45:11 -0300

    primeira função em javascript
    adicionei a função de passar das setas.

---
+++ index.js
@@ -0,0 +1,33 @@
+let contador=0;
+
+
+//Função responsável pelo funcionamento das setas:
+function passar(){
+    let aula = document.getElementById('aulas');
+    let cronos = document.getElementById('cronos');
+    let estatics = document.getElementById('estatic');
+    
+    
+   contador++;
+   
+   if(contador == 1){
+            aula.style.display = "block";
+            cronos.style.display = "none";
+            estatics.style.display = "none";
+            
+        }
+        else if(contador == 2){
+            aula.style.display = "none";
+            cronos.style.display = "block";
+            estatics.style.display = "none";
+
+        }else if(contador == 3){
+            aula.style.display = "none";
+            cronos.style.display = "none";
+            estatics.style.display = "block";
+        }
+        else if(contador > 3){
+            contador = 0;
+        }
+}
+
